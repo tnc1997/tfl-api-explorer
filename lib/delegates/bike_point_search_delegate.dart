836b1e8abb89d6ed52a706e1972b36626832a747
Thomas Clark <tnc1997@virginmedia.com>
2019-09-07 16:38:47 +0100

    fix: search delegate app bar theme (#10)

--- lib/delegates/bike_point_search_delegate.dart
+++ lib/delegates/bike_point_search_delegate.dart
@@ -10,6 +10,28 @@ class BikePointSearchDelegate extends SearchDelegate<Place> {
   BikePointSearchDelegate(this._bikePoints);
 
   @override
+  ThemeData appBarTheme(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return theme.copyWith(
+      inputDecorationTheme: InputDecorationTheme(
+        hintStyle: TextStyle(
+          color: theme.primaryTextTheme.title.color,
+        ),
+      ),
+      primaryColor: theme.primaryColor,
+      primaryIconTheme: theme.primaryIconTheme,
+      primaryColorBrightness: theme.primaryColorBrightness,
+      primaryTextTheme: theme.primaryTextTheme,
+      textTheme: theme.textTheme.copyWith(
+        title: theme.textTheme.title.copyWith(
+          color: theme.primaryTextTheme.title.color,
+        ),
+      ),
+    );
+  }
+
+  @override
   List<Widget> buildActions(BuildContext context) {
     return <Widget>[
       IconButton(
