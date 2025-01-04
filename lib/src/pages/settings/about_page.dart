a4d9ee30de1a7c0f839d768809c718e768583431
Thomas Clark <tnc1997@outlook.com>
2025-01-04 08:38:56 +0000

    refactor: replace package_info with package_info_plus

--- lib/src/pages/settings/about_page.dart
+++ lib/src/pages/settings/about_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:tfl_api_explorer/src/widgets/circular_progress_indicator_future_builder.dart';
 
 class AboutPage extends StatefulWidget {
