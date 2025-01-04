b99523e879ad3688869fac792bc83de962e4485b
Thomas Clark <tnc1997@outlook.com>
2025-01-04 14:15:03 +0000

    refactor: update tfl colors

--- lib/src/colors/tfl_colors.dart
+++ lib/src/colors/tfl_colors.dart
@@ -1,129 +1,58 @@
 import 'package:flutter/material.dart';
 
 class TflColors {
-  static const blue = MaterialColor(
-    _bluePrimaryValue,
-    <int, Color>{
-      50: Color(0xFFE0E3F5),
-      100: Color(0xFFB3BAE5),
-      200: Color(0xFF808CD4),
-      300: Color(0xFF4D5EC2),
-      400: Color(0xFF263CB5),
-      500: Color(_bluePrimaryValue),
-      600: Color(0xFF0016A0),
-      700: Color(0xFF001297),
-      800: Color(0xFF000E8D),
-      900: Color(0xFF00087D),
-    },
-  );
-  static const _bluePrimaryValue = 0xFF0019A8;
+  static const corporateBlack = Color(0xff000000);
+  static const corporateBlue = Color(0xff0019a8);
+  static const corporateDarkGrey = Color(0xff323e48);
+  static const corporateGreen = Color(0xff007834);
+  static const corporateGrey = Color(0xff838d93);
+  static const corporateRed = Color(0xffe1251b);
+  static const corporateWhite = Color(0xffffffff);
+  static const corporateYellow = Color(0xffffcd00);
 
-  static const blueAccent = MaterialAccentColor(
-    _blueAccentValue,
-    <int, Color>{
-      100: Color(0xFFAAACFF),
-      200: Color(_blueAccentValue),
-      400: Color(0xFF4447FF),
-      700: Color(0xFF2A2EFF),
-    },
-  );
-  static const _blueAccentValue = 0xFF7779FF;
+  static const docklandsLightRailway = Color(0xff00afad);
+  static const elizabethLine = Color(0xff60399e);
+  static const londonBuses = Color(0xffdc241f);
+  static const londonCableCar = Color(0xff734fa0);
+  static const londonCoaches = Color(0xffffa600);
+  static const londonDialARide = Color(0xffc028b9);
+  static const londonOverground = Color(0xfffa7b05);
+  static const londonRiverServices = Color(0xff039ae5);
+  static const londonTrams = Color(0xff5fb526);
+  static const londonUnderground = Color(0xff0019a8);
+  static const santanderCycles = Color(0xffef3824);
 
-  static const blueGrey = MaterialColor(
-    _blueGreyPrimaryValue,
-    <int, Color>{
-      50: Color(0xFFE6E6E7),
-      100: Color(0xFFC0C1C4),
-      200: Color(0xFF96989C),
-      300: Color(0xFF6C6E74),
-      400: Color(0xFF4D4F57),
-      500: Color(_blueGreyPrimaryValue),
-      600: Color(0xFF282B33),
-      700: Color(0xFF22242C),
-      800: Color(0xFF1C1E24),
-      900: Color(0xFF111317),
-    },
-  );
-  static const _blueGreyPrimaryValue = 0xFF2D3039;
+  static const bakerlooLine = Color(0xffb26300);
+  static const centralLine = Color(0xffdc241f);
+  static const circleLine = Color(0xffffc80a);
+  static const districtLine = Color(0xff007d32);
+  static const hammersmithAndCityLine = Color(0xfff589a6);
+  static const jubileeLine = Color(0xff838d93);
+  static const metropolitanLine = Color(0xff9b0058);
+  static const northernLine = Color(0xff000000);
+  static const piccadillyLine = Color(0xff0019a8);
+  static const victoriaLine = Color(0xff039be5);
+  static const waterlooAndCityLine = Color(0xff76d0bd);
 
-  static const blueGreyAccent = MaterialAccentColor(
-    _blueGreyAccentValue,
-    <int, Color>{
-      100: Color(0xFF5D9EFF),
-      200: Color(_blueGreyAccentValue),
-      400: Color(0xFF0062F6),
-      700: Color(0xFF0058DC),
-    },
-  );
-  static const _blueGreyAccentValue = 0xFF2A7FFF;
+  static const libertyLine = Color(0xff5d6061);
+  static const lionessLine = Color(0xfffaa61a);
+  static const mildmayLine = Color(0xff0077ad);
+  static const suffragetteLine = Color(0xff5bbd72);
+  static const weaverLine = Color(0xff823a62);
+  static const windrushLine = Color(0xffed1b00);
 
-  static const red = MaterialColor(
-    _redPrimaryValue,
-    <int, Color>{
-      50: Color(0xFFFBE5E4),
-      100: Color(0xFFF5BDBC),
-      200: Color(0xFFEE928F),
-      300: Color(0xFFE76662),
-      400: Color(0xFFE14541),
-      500: Color(_redPrimaryValue),
-      600: Color(0xFFD8201B),
-      700: Color(0xFFD31B17),
-      800: Color(0xFFCE1612),
-      900: Color(0xFFC50D0A),
-    },
-  );
-  static const _redPrimaryValue = 0xFFDC241F;
+  static const superloopCyan = Color(0xff37abdd);
+  static const superloopGreen = Color(0xffbbcc00);
+  static const superloopMaroon = Color(0xffbe005e);
+  static const superloopOrange = Color(0xfff29500);
+  static const superloopRed = Color(0xffdc241f);
 
-  static const MaterialAccentColor redAccent = MaterialAccentColor(
-    _redAccentValue,
-    <int, Color>{
-      100: Color(0xFFFFF0F0),
-      200: Color(_redAccentValue),
-      400: Color(0xFFFF8B8A),
-      700: Color(0xFFFF7270),
-    },
-  );
-  static const _redAccentValue = 0xFFFFBDBD;
-
-  static const turquoise = MaterialColor(
-    _turquoisePrimaryValue,
-    <int, Color>{
-      50: Color(0xFFEDF9F9),
-      100: Color(0xFFD1F0F0),
-      200: Color(0xFFB3E6E6),
-      300: Color(0xFF94DBDB),
-      400: Color(0xFF7DD4D4),
-      500: Color(_turquoisePrimaryValue),
-      600: Color(0xFF5EC7C7),
-      700: Color(0xFF53C0C0),
-      800: Color(0xFF49B9B9),
-      900: Color(0xFF38ADAD),
-    },
-  );
-  static const _turquoisePrimaryValue = 0xFF66CCCC;
-
-  static const turquoiseAccent = MaterialAccentColor(
-    _turquoiseAccentValue,
-    <int, Color>{
-      100: Color(0xFFF9FFFF),
-      200: Color(_turquoiseAccentValue),
-      400: Color(0xFF93FFFF),
-      700: Color(0xFF7AFFFF),
-    },
-  );
-  static const _turquoiseAccentValue = 0xFFC6FFFF;
-
-  static const List<MaterialColor> primaries = <MaterialColor>[
-    blue,
-    blueGrey,
-    red,
-    turquoise,
-  ];
-
-  static const List<MaterialAccentColor> accents = <MaterialAccentColor>[
-    blueAccent,
-    blueGreyAccent,
-    redAccent,
-    turquoiseAccent,
-  ];
+  static const congestionCharge = Color(0xffce0e2d);
+  static const cycleway = Color(0xff80bc00);
+  static const legibleLondonBlue = Color(0xff002043);
+  static const legibleLondonYellow = Color(0xfff0bc0b);
+  static const lowEmissionZone = Color(0xff5e9732);
+  static const oysterCyan = Color(0xff0ea9ec);
+  static const oysterDarkBlue = Color(0xff0019a8);
+  static const visitorCentre = Color(0xffec008c);
 }
