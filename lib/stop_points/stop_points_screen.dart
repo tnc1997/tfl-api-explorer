d885d13739590480013b6ae37b907dd345636c42
Thomas Clark <tnc1997@outlook.com>
2025-01-14 18:46:10 +0000

    refactor(stop-points): extract stop point filters page

--- lib/stop_points/stop_points_screen.dart
+++ lib/stop_points/stop_points_screen.dart
@@ -5,6 +5,7 @@ import 'package:tfl_api_client/tfl_api_client.dart';
 import '../common/circular_progress_indicator_future_builder.dart';
 import '../common/drawer.dart';
 import 'stop_point_filters_notifier.dart';
+import 'stop_point_filters_page.dart';
 import 'stop_point_list_tile.dart';
 
 class StopPointsScreen extends StatefulWidget {
@@ -33,7 +34,7 @@ class _StopPointsScreenState extends State<StopPointsScreen> {
               await Navigator.of(context).push(
                 MaterialPageRoute(
                   builder: (context) {
-                    return _StopPointFiltersPage();
+                    return const StopPointFiltersPage();
                   },
                 ),
               );
@@ -73,76 +74,5 @@ class _StopPointsScreenState extends State<StopPointsScreen> {
     }
 
     _future = context.read<TflApiClient>().stopPoint.getByMode(modes, page: 1);
-  }
-}
-
-class _StopPointFiltersPage extends StatefulWidget {
-  @override
-  State<_StopPointFiltersPage> createState() {
-    return _StopPointFiltersPageState();
-  }
-}
-
-class _StopPointFiltersPageState extends State<_StopPointFiltersPage> {
-  late final Future<List<Mode>> _future;
-
-  @override
-  Widget build(BuildContext context) {
-    final notifier = context.watch<StopPointFiltersNotifier>();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Filters'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.restore),
-            onPressed: () {
-              notifier.reset();
-            },
-          ),
-        ],
-      ),
-      body: CircularProgressIndicatorFutureBuilder<List<Mode>>(
-        future: _future,
-        builder: (context, data) {
-          if (data != null) {
-            return ListView(
-              children: <Widget>[
-                ExpansionTile(
-                  title: Text('Modes'),
-                  children: [
-                    for (final mode in data)
-                      if (mode.modeName case final modeName?)
-                        CheckboxListTile(
-                          value: notifier.modes.contains(modeName),
-                          onChanged: (value) {
-                            if (value == true) {
-                              notifier.addMode(modeName);
-                            } else {
-                              notifier.removeMode(modeName);
-                            }
-                          },
-                          title: Text(
-                            modeName,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                  ],
-                ),
-              ],
-            );
-          } else {
-            return Container();
-          }
-        },
-      ),
-    );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    _future = context.read<TflApiClient>().stopPoint.metaModes();
   }
 }
